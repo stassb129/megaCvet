9c8320cc8c6d2c37348cbb80fdb95add6b9f3856
artandsky <krekerr85@gmail.com>
2020-11-12 23:26:26 +0300

    Изменил подключение к бд

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const exphbs = require('express-handlebars')
-const todoRouters = require('./routes/todos')
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const todoRouters = require("./routes/todos");
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
+	defaultLayout: "main",
+	extname: "hbs",
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(todoRouters)
+app.use(todoRouters);
 
 async function start() {
-    try {
-        await mongoose.connect('mongodb+srv://stas:stassb129@cluster0.umg6s.mongodb.net/todos', {
-            useNewUrlParser: true,
-            useFindAndModify: false
-        })
+	try {
+		await mongoose.connect(
+			"mongodb+srv://artandsky:1525402hj@cluster0.vcohb.mongodb.net/artandsky?retryWrites=true&w=majority",
+			{
+				useNewUrlParser: true,
+				useFindAndModify: false,
+			}
+		);
 
-        app.listen(PORT, () => {
-            console.log('Server has been started...')
-        })
-    } catch (e) {
-        console.log(e)
-    }
+		app.listen(PORT, () => {
+			console.log("Server has been started...");
+		});
+	} catch (e) {
+		console.log(e);
+	}
 }
 
-start()
+start();
